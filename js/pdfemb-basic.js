56597aa19038d27c82f630bacdfc8599d6ecdb77
danlester <danlester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-20 17:24:57 +0000

    2.1 Site-wide settings can be set by the user
    git-svn-id: https://plugins.svn.wordpress.org/pdf-embedder/tags/2.1@1164367 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/pdfemb-basic.js
@@ -0,0 +1,16 @@
+
+function pdfembGetPDF(url, callback) {
+	    	
+	callback(url, false);
+
+};
+
+function pdfembWantMobile($, divContainer, wantWidth, wantHeight) {
+	return false;
+}
+
+function pdfembMakeMobile($, wantMobile, innerdiv) {
+}
+
+function pdfembAddMoreToolbar($, toolbar, divContainer) {
+}
